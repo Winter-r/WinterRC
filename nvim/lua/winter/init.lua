069fffdc1dddbdec2c5e68d5b9b8b4dc7c89f970
Mohamed Elsherif <mohamedelsherif.mo123@gmail.com>
2024-08-22 14:49:21 +0400

    Initial Commit

---
+++ nvim/lua/winter/init.lua
@@ -0,0 +1,3 @@
+require("winter.remap")
+require("winter.lazy")
+print("Welcome Back, Sir")
---
+++ nvim/lua/winter/lazy.lua
@@ -0,0 +1,55 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	{
+		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+	},
+	'mbbill/undotree',
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{
+		"tiagovla/tokyodark.nvim",
+		opts = {
+			transparent_background = true, -- set background to transparent
+			gamma = 1.00, -- adjust the brightness of the theme
+			styles = {
+				comments = { italic = true }, -- style for comments
+				keywords = { italic = true }, -- style for keywords
+				identifiers = { italic = true }, -- style for identifiers
+				functions = {}, -- style for functions
+				variables = {}, -- style for variables
+			},
+			custom_highlights = {} or function(highlights, palette) return {} end, -- extend highlights
+			custom_palette = {} or function(palette) return {} end, -- extend palette
+			terminal_colors = true, -- enable terminal colors		
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts) -- calling setup is optional
+			vim.cmd [[colorscheme tokyodark]]
+		end,
+	},
+	{
+		'github/copilot.vim'
+	},
+	{
+		'tpope/vim-fugitive'
+	},
+})
